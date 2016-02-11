9cae6257147fb29a70126bab44eb32f8f85a3bd6
Jure Ratkovic <ratkovic.jure@gmail.com>
2016-02-11 11:59:45 +0100

    Light structure refactor

--- Engine/Engine/Light.cpp
+++ Engine/Engine/Light.cpp
@@ -4,10 +4,64 @@
 
 #include <Engine/Engine/Light.h>
 
+using namespace glm;
+
 namespace engine
 {
-    Light::Light(void)
-        : mSpotCutoff(0.0f), mSpotExponent(0.0f), mFlag(LIGHT_DIRECTIONAL), mIsEnabled(true)
+    Light::Light(const vec3 &intensity, LightType type, bool isActive)
+        : mIntensity(intensity), mType(type), mIsActive(isActive)
     {
     }
+
+    Light::~Light()
+    {
+    }
+
+    void Light::ApplyToProgram(const Program &prog) const
+    {
+        prog.SetUniform("Light.intensity", mIntensity);
+    }
+
+    bool Light::operator<(const Light *other) const
+    {
+        return mType < other->mType;
+    }
+
+    DirectionalLight::DirectionalLight(const vec3 &intensity, bool isActive)
+        : Light(intensity, LightType::DIRECTIONAL, isActive)
+    {
+    }
+
+    void DirectionalLight::ApplyToProgram(const Program &prog) const
+    {
+        Light::ApplyToProgram(prog);
+    }
+
+    PointLight::PointLight(const vec3 &intensity, bool isActive, const vec3 &attenuation, const vec3 &position)
+        : Light(intensity, LightType::POINT, isActive), mAttenuation(attenuation), mPosition(position)
+    {
+    }
+
+    void PointLight::ApplyToProgram(const Program &prog) const
+    {
+        Light::ApplyToProgram(prog);
+        prog.SetUniform("Light.attenuation", mAttenuation);
+        prog.SetUniform("Light.position", mPosition);
+    }
+
+    SpotLight::SpotLight(const vec3 &intensity, bool isActive, const vec3 &attenuation, const vec3 &position, const vec3 &spotDir, float spotCutoff, float spotExp)
+        : Light(intensity, LightType::SPOT, isActive), mAttenuation(attenuation), mPosition(position), mSpotDirection(spotDir), mSpotCutoff(spotCutoff), mSpotExponent(spotExp)
+    {
+    }
+
+    void SpotLight::ApplyToProgram(const Program &prog) const
+    {
+        Light::ApplyToProgram(prog);
+        prog.SetUniform("Light.attenuation", mAttenuation);
+        prog.SetUniform("Light.position", mPosition);
+        prog.SetUniform("Light.spotDir", mSpotDirection);
+        prog.SetUniform("Light.spotCutoff", mSpotCutoff);
+        prog.SetUniform("Light.spotExp", mSpotExponent);
+    }
+
 }
--- Engine/Engine/Light.h
+++ Engine/Engine/Light.h
@@ -1,40 +1,75 @@
-/*
-* Copyright (c) 2014 Jure Ratkovic
-*/
 
 #ifndef EN_LIGHT_H
 #define EN_LIGHT_H
 
-#include <glm/glm.hpp>
+#include <Engine/GL/Program.h>
 
 namespace engine
 {
-    enum LightFlag
+    enum LightType
     {
-        LIGHT_NONE = 0,
-        LIGHT_DIRECTIONAL = 1 << 0,
-        LIGHT_POINT = 1 << 1,
-        LIGHT_SPOT = 1 << 2,
-
-        LIGHT_CASTS_SHADOWS = 1 << 3
+        DIRECTIONAL = 0,
+        POINT,
+        SPOT,
     };
 
     struct Light
     {
-        Light(void);
+        Light(const glm::vec3 &intensity, LightType type, bool isActive);
+        virtual ~Light();
+
+        // Call this from derived functions
+        virtual void ApplyToProgram(const Program &prog) const = 0;
+
+        bool operator<(const Light* other) const;
 
         glm::vec3 mIntensity;
+        LightType mType;
+        bool mIsActive;
+    };
+
+    struct DirectionalLight : public Light
+    {
+        DirectionalLight(const glm::vec3 &intensity, bool isActive);
+
+        virtual void ApplyToProgram(const Program &prog) const override;
+    };
+
+    struct PointLight : public Light
+    {
+        PointLight(const glm::vec3 &intensity, 
+            bool isActive,
+            const glm::vec3 &attenuation,
+            const glm::vec3 &position);
+
+        virtual void ApplyToProgram(const Program &prog) const override;
+
         //Constant, linear, quadratic
         glm::vec3 mAttenuation;
         //This is direction is the light is directional
         glm::vec3 mPosition;
-        //These can be disregarded if light isn't spot light
+    };
+
+    struct SpotLight : public Light
+    {
+        SpotLight(const glm::vec3 &intensity,
+            bool isActive,
+            const glm::vec3 &attenuation,
+            const glm::vec3 &position,
+            const glm::vec3 &spotDir,
+            float spotCutoff,
+            float spotExp);
+
+        virtual void ApplyToProgram(const Program &prog) const override;
+
+        //Constant, linear, quadratic
+        glm::vec3 mAttenuation;
+        //This is direction is the light is directional
+        glm::vec3 mPosition;
+        // SpotLight specific
         glm::vec3 mSpotDirection;
         float mSpotCutoff;
         float mSpotExponent;
-
-        LightFlag mFlag;
-        bool mIsEnabled;
     };
 }
 
