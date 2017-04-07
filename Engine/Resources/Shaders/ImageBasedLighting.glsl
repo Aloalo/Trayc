389fae7ba26927dd983c11d312c888e741570b32
Jure Ratkovic <ratkovic.jure@gmail.com>
2017-04-07 20:02:56 +0200

    Irradiance map shading fix

--- Engine/Resources/Shaders/ImageBasedLighting.glsl
+++ Engine/Resources/Shaders/ImageBasedLighting.glsl
@@ -7,12 +7,13 @@ uniform mat4 cubemapM;
 
 vec3 IBL(in vec3 V, in vec3 N, in float roughness, in vec3 F0, in vec3 albedo)
 {
+    // Diffuse contribution
+    vec3 sampleDir = normalize((invV * vec4(N, 0.0)).xyz);
+    vec3 irradiance = texture(irradianceMap, sampleDir).rgb;
+    
     // Calculate and transform the reflection vector
     vec3 R = reflect(-V, N);
     R = (cubemapM * (invV * vec4(R, 0.0))).xyz;
-    
-    // Diffuse contribution
-    vec3 irradiance = texture(irradianceMap, R).rgb;
     
     // Specular contribution
     float lod = log2(float(textureSize(reflectionMap, 0).x));
